ef88f8e75874731b2d660c1e63b853ab91c08788
Devdoot Ghosh <devdoott@gmail.com>
2017-04-11 19:43:50 +0530

    minor change

--- demo.m
+++ demo.m
@@ -4,26 +4,7 @@ for j=1:1
 for i=1:1
     
     x=strcat('rec_',int2str(i))
-   [Entropy,esmooth,stemp,stemps]=spect(x,y);
-   EntropyNorm=[EntropyNorm;Entropy];
-   esmoothNorm=[esmoothNorm;esmooth];
-   stempNorm=[stempNorm;stemp];
-   stempsNorm=[stempsNorm;stemps];
-   
-    h=[h;correlate(P,x,y)];
-    M=probMat(x,y);
-    RRAF=[RRAF;M(2,2)];
-        [D E]=HRSimilarity(x,y);
-    DKLAF=[DKLAF;D];
-    EAF=[EAF;E];
-    typ=[typ;0];
-    %if(j==1)ttSmooth=amp
-    %else ttSmooth=ttSmooth+amp;
-  % end
-   % length(f)
-   % length(amp)
-   %figure(j) 
-   %plot(f,amp)
+  
     %hold on
     [output input ecgPlot]=processing(x,y)
     %input=[DKLAF';EAF';EntropyNorm';RRAF';h';stempNorm']
